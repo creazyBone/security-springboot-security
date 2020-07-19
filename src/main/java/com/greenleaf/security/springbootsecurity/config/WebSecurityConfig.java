be89f82c4676e184a9a143a77e016cbd17514298
Neo <kwaad2009@163.com>
2020-07-19 21:40:56 +0800

    3rd commit

--- src/main/java/com/greenleaf/security/springbootsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/greenleaf/security/springbootsecurity/config/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)//这里需要通过会话来保存认证信息，所以用required。如果通过token的方式来保存认证信息，则可以设置成stateless
             .and()
                 .logout()//提供系统推出支持，使用WebSecurityConfigurerAdapter会自动被应用
-                .logoutUrl("/logout")//设置触发退出操作的URL
+                .logoutUrl("/logout")//设置触发退出操作的URL。
                 .logoutSuccessUrl("/login-view?logout");//退出之后跳转的URL，默认是/logout?logout. ?logout仅仅是表示退出，没有实际意义。 用了.anyRequest().authenticated()会要求logoutSuccessUrl认证后才可以访问，所以会强制重定向到跳转到登录页
                 //.logoutSuccessHandler(logoutSuccessHandler)//定制的LogoutSuccessHandler，用于实现用户退出成功时的处理。如果配了这个，logoutSuccessUrl的设置会被忽略
                 //.addLogoutHandler(new CookieClearingLogoutHandler())//添加一个logoutHandler，用于实现用户退出（不管退出成功不成功）时的清理工作。默认SecurityContextLogoutHandler会被添加成最后一个LogoutHandler
